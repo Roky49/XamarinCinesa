08cabe9f7f05ee8b04b5b3082b8424660804702a
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-05-27 11:45:59 +0200

    add ViewPelicula and ViewModelPelicula

---
+++ AppCinema/AppCinema/ViewModel/ViewModelPelicula.cs
@@ -0,0 +1,32 @@
+﻿using AppCinema.Base;
+using AppCinema.Models;
+using AppCinema.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCinema.ViewModel
+{
+    public class ViewModelPelicula : ViewModelBase
+    {
+        RepositoryMovie repoMovie;
+        private Movie _Movie;
+        public Movie Movie
+        {
+            get { return this._Movie; }
+            set
+            {
+                this._Movie = value;
+                OnPropertyChanged("Movie");
+            }
+        }
+        public ViewModelPelicula(int movieId)
+        {
+            repoMovie = new RepositoryMovie();
+            Task.Run(async() => {
+                this.Movie = await repoMovie.GetMovie(movieId);
+            });
+        }
+    }
+}

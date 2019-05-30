3c742ce3ec3e73487afffa25d17875946b98b366
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-05-30 14:23:57 +0200

    add commands to buttons

--- AppCinema/AppCinema/ViewModel/ViewModelPelicula.cs
+++ AppCinema/AppCinema/ViewModel/ViewModelPelicula.cs
@@ -6,12 +6,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace AppCinema.ViewModel
 {
     public class ViewModelPelicula : ViewModelBase
     {
         RepositoryMovie repoMovie;
+        RepositoryCinema repoCine;
+        SessionService session;
+        public bool InList { get; set; }
         private Movie _Movie;
         public Movie Movie
         {
@@ -44,19 +48,38 @@ namespace AppCinema.ViewModel
                 OnPropertyChanged("Cast");
             }
         }
-        public ViewModelPelicula()
+        public Command AddToList()
         {
-            repoMovie = new RepositoryMovie();
-            InstanceCarouselPeople();
-            Task.Run(async() => {
-                this.Movie = await repoMovie.GetMovie(121);
+            return new Command(async() => {
+                if(session.Email != null)
+                {
+                    await repoCine.AddMovieToList(Movie.ID, session.Email);
+                    await Application.Current.MainPage.DisplayAlert("Pelicula añadida a tu lista", "ejemplo", "OK");
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("inicia sesion para añadir peliculas", "ejemplo", "OK");
+                }
             });
         }
-        private void InstanceCarouselPeople()
+        public Command RemoveFromList()
         {
-            List<Cast> initialCast = new List<Cast>();
-            initialCast.Add(new Cast() { ID = 0, Name = "John Doe", Character = "John Doe", ProfilePath = "" });
-            this.Cast = new ObservableCollection<Cast>(initialCast);
+            return new Command(async () => {
+                if (session.Email != null)
+                    await repoCine.RemoveMovieFromList(Movie.ID, session.Email);
+            });
+        }
+        public ViewModelPelicula()
+        {
+            InList = false;
+            repoMovie = new RepositoryMovie();
+            repoCine = new RepositoryCinema();
+            session = App.Locator.SessionService;
+            Task.Run(async() => {
+                this.Movie = await repoMovie.GetMovie(121);
+                if(session.Email != null)
+                    this.InList = await repoCine.CheckInList(121, session.Email);
+            });
         }
     }
 }

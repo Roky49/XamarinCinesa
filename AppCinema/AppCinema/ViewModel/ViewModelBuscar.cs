f8f0f2f2d98904fb2b06a5cfb998d17873bc9b49
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-05-31 14:14:30 +0200

    add movie list property and costructor to viewmodel

--- AppCinema/AppCinema/ViewModel/ViewModelBuscar.cs
+++ AppCinema/AppCinema/ViewModel/ViewModelBuscar.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using AppCinema.Base;
+using AppCinema.Models;
+using AppCinema.Repositories;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AppCinema.ViewModel
 {
-    class ViewModelBuscar
+    public class ViewModelBuscar : ViewModelBase
     {
+        RepositoryMovie repoMovie;
+        private ObservableCollection<DiscoverMovie> _Movies;
+        public ObservableCollection<DiscoverMovie> Movies
+        {
+            get { return this._Movies; }
+            set
+            {
+                this._Movies = value;
+                OnPropertyChanged("Movies");
+            }
+        }
+        public ViewModelBuscar()
+        {
+            repoMovie = new RepositoryMovie();
+            Task.Run(async() => {
+                DiscoverMovieRequest request = await repoMovie.SearchMovie("avengers");
+                this.Movies = new ObservableCollection<DiscoverMovie>(request.Movies);
+            });
+        }
     }
 }
